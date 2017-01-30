8089dd3ed2c1ead48e44c2f4833023957a48ce4a
shyovn <shyovn@gmail.com>
2017-01-30 04:48:37 +0000

    updated difficulty on health

--- python_bible/health_potion.py
+++ python_bible/health_potion.py
@@ -2,7 +2,8 @@
 import random
 
 health = 50
-potion_health = random.randint(25, 50)
+difficulty = 2
+potion_health = int(random.randint(25, 50) / difficulty)
 
 health = health + potion_health
 print health
