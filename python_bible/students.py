f825b2cc9615287caefa3db64ed2ab24a2e4a87c
shyovn <shyovn@gmail.com>
2017-02-05 23:06:10 +0000

    basic dict pract

---
+++ python_bible/students.py
@@ -0,0 +1,13 @@
+students = {
+            "Alice"
+            :{"id":"ID1", "age":26, "grade":"A"},
+            "Vincent"
+            :{"id":"ID2", "age":36, "grade":"B"},
+            "Sthephen"
+            :{"id":"ID3", "age":46, "grade":"C"},
+            "Mike"
+            :{"id":"ID4", "age":56, "grade":"D"},
+            }
+print(students["Alice"]["age"])
+print(students["Vincent"]["grade"])
+
